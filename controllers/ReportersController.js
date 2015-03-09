cc0e8c9a47bc0a7b09463d8a54b46c752912b294
Shimran <Shimran@nyu.edu>
2015-03-09 16:32:31 -0700

    Correct bug

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,5 +1,6 @@
 newspaper.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams, BeatsFactory, UtilitiesFactory) {
-  $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId)
+  $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId);
+
   $scope.addReporter = function() {
     $scope.beat.reporters.push({ name: $scope.reporterName, id: $scope.beat.reporters.length +1, });
     $scope.reporterName = null;
@@ -7,9 +8,9 @@ newspaper.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParam
   $scope.editReporter = function(id) {
 
     $scope.reporter = UtilitiesFactory.findById($scope.beat.reporters, id)
-    $scope.reporter.tenure = $scope.reporterTenure;
-    $scope.reporter.birthday = $scope.reporterBirthday;
-
+    $scope.reporter.tenure = this.reporterTenure;
+    $scope.reporter.birthday = this.reporterBirthday;
+    
     $scope.reporterTenure = null;
     $scope.reporterBirthday = null;
   };
